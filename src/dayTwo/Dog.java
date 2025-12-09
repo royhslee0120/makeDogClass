d04451b6e5bdd3d42ce0a24771559687c04f982a
royhsle0120 <royhslee0120@gmail.com>
2025-12-09 19:22:47 +0900

    강아지 클래스 만들기 완료

--- src/dayTwo/Dog.java
+++ src/dayTwo/Dog.java
@@ -1,4 +1,12 @@
 package dayTwo;
 
 public class Dog {
+
+    String name;
+    String breed;
+
+    public Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
 }
--- src/dayTwo/Main.java
+++ src/dayTwo/Main.java
@@ -1,4 +1,15 @@
 package dayTwo;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("멍멍이", "말티즈");
+        Dog dog2 = new Dog("초코", "진돗개");
+        Dog dog3 = new Dog("뭉치", "푸들");
+
+        System.out.println(dog1.name + " " + dog1.breed);
+        System.out.println(dog2.name + " " + dog2.breed);
+        System.out.println(dog3.name + " " + dog3.breed);
+    }
 }
