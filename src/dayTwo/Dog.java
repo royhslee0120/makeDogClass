9f82ce0a42862829f3bf67af4dcf91530ccb4a3f
royhsle0120 <royhslee0120@gmail.com>
2025-12-12 19:38:13 +0900

    자유롭게 메서드 정의하기

--- src/dayTwo/Dog.java
+++ src/dayTwo/Dog.java
@@ -9,4 +9,13 @@ public class Dog {
         this.name = name;
         this.breed = breed;
     }
+
+    public void bark() {
+        System.out.println(name + "가 짖습니다.");
+    }
+
+    public void wag() {
+        System.out.println(name + "꼬리를 흔듭니다.");
+    }
+
 }
