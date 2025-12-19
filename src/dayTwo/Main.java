c54443cf63606fec966b9f2c297bba3f11b05943
royhsle0120 <royhslee0120@gmail.com>
2025-12-19 20:55:18 +0900

    고양이 클래스 추가

--- src/dayTwo/Main.java
+++ src/dayTwo/Main.java
@@ -8,9 +8,17 @@ public class Main {
         Dog dog2 = new Dog("초코", "진돗개");
         Dog dog3 = new Dog("뭉치", "푸들");
 
+        Cat cat1 = new Cat("피치", "샴");
+        Cat cat2 = new Cat("야옹이", "먼치킨");
+        Cat cat3 = new Cat("베르샤유", "페르시안");
+
         System.out.println(dog1.getName() + " / " + dog1.getBreed());
         System.out.println(dog2.getName() + " / " + dog2.getBreed());
         System.out.println(dog3.getName() + " / " + dog3.getBreed());
+
+        System.out.println(cat1.getName() + " / " + cat1.getBreed());
+        System.out.println(cat2.getName() + " / " + cat2.getBreed());
+        System.out.println(cat3.getName() + " / " + cat3.getBreed());
 
         dog1.bark();
         dog2.bark();
@@ -19,5 +27,14 @@ public class Main {
         dog1.wag();
         dog2.wag();
         dog3.wag();
+
+        cat1.cry();
+        cat2.cry();
+        cat3.cry();
+
+        cat1.scratch();
+        cat2.scratch();
+        cat3.scratch();
+
     }
 }
